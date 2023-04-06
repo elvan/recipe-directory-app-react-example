8d516ca72e2f94d160422064b737e081ff1ca668
Elvan Hidayat <elvanhd@gmail.com>
2023-04-07 01:36:10 +0700

    search results page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,6 +1,24 @@
+import { useLocation } from 'react-router-dom';
+import RecipeList from '../../components/RecipeList';
+import { useFetch } from '../../hooks/useFetch';
+
 // styles
 import './Search.css';
 
 export default function Search() {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get('q');
+
+  const url = 'http://localhost:3000/recipes?q=' + query;
+  const { error, isPending, data } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className='page-title'>Recipes including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
