c668c99906a167854ef0f6a132e4897d8bb39ca9
Elvan Hidayat <elvanhd@gmail.com>
2023-04-07 01:13:36 +0700

    making a search bar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,16 @@ import { Link } from 'react-router-dom';
 
 // styles
 import './Navbar.css';
+import Searchbar from './Searchbar';
 
 export default function Navbar() {
   return (
     <div className='navbar'>
       <nav>
         <Link to='/' className='brand'>
-          <h1>RecipeDirectory</h1>
+          <h1>Cooking Ninja</h1>
         </Link>
+        <Searchbar />
         <Link to='/create'>Create Recipe</Link>
       </nav>
     </div>
