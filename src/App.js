f7e650c7dfc5c7951a0b23161fb5724a1764da21
Elvan Hidayat <elvanhd@gmail.com>
2023-04-07 18:49:09 +0700

    making a color theme selector

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // page components
 import Navbar from './components/Navbar';
+import ThemeSelector from './components/ThemeSelector';
 import Create from './pages/create/Create';
 import Home from './pages/home/Home';
 import Recipe from './pages/recipe/Recipe';
@@ -15,6 +16,7 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Navbar />
+        <ThemeSelector />
         <Switch>
           <Route exact path='/'>
             <Home />
