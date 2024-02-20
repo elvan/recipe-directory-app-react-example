f9e87e7746028c30d5cfbde8d28c52831a3774f9
Elvan Hidayat <elvanhd@gmail.com>
2024-02-20 07:10:59 +0700

    Integrate routing and create basic page components
    - Install react-router-dom for page navigation
    - Update App.js to include BrowserRouter and routes
    - Create Navbar component with navigation links
    - Add empty CSS files for future styling of components
    - Implement basic structure for Create, Home, Recipe, and Search pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// page components
+import Navbar from './components/Navbar';
+import Create from './pages/create/Create';
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
+// styles
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/create'>
+            <Create />
+          </Route>
+          <Route path='/search'>
+            <Search />
+          </Route>
+          <Route path='/recipes/:id'>
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
