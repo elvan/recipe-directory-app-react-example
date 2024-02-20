f99e11491b9f3574cfb2bdbbded5acd2d0494798
Elvan Hidayat <elvanhd@gmail.com>
2024-02-20 07:52:30 +0700

    Implement Create Recipe form with basic styling
    - Add form to Create page for new recipe submission
    - Use useState hooks to manage form inputs for title, method, and cooking time
    - Style Create page for better user experience and readability
    - Include handleSubmit function to log input values for now

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,6 +1,46 @@
+import { useState } from 'react';
+
 // styles
 import './Create.css';
 
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState('');
+  const [method, setMethod] = useState('');
+  const [cookingTime, setCookingTime] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, method, cookingTime);
+  };
+
+  return (
+    <div className='create'>
+      <h2 className='page-title'>Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe title:</span>
+          <input type='text' onChange={(e) => setTitle(e.target.value)} value={title} required />
+        </label>
+
+        {/* recipe ingredients here */}
+
+        <label>
+          <span>Recipe Method:</span>
+          <textarea onChange={(e) => setMethod(e.target.value)} value={method} required />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type='number'
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className='btn'>submit</button>
+      </form>
+    </div>
+  );
 }
